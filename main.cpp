964a4de7e00557eba84b671c6b4c7e7865120064
HenryNavarro1998 <henrymanuelnavarro@gmail.com>
2022-07-25 16:48:13 -0400

    First Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/****************************************************
+* Henry Manuel Navarro                         		*
+* V-28.031.608                                   	*
+* ESTRUCTURA DE DATOS                               *
+* SECCIÓN 01                                        *
+* PRACTICA #2, 'Listas, Pilas y Colas'.				*
+*****************************************************/
+
+//Librerias Incluidas
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+//Funcion Main
+int main()
+{
+    cout << "\nHello World" << endl;
+
+    return 0;
+}
